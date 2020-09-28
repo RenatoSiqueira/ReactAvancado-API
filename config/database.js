e97b623ee761276f9690bc911dfed251f2a6e8df
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-09-28 00:06:31 -0300

    save

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -5,11 +8,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'postgres'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'docker'),
+        host: config.host || '127.0.0.1',
+        port: config.port || 5432,
+        database: config.database || 'postgres',
+        username: config.user || 'postgres',
+        password: config.password || 'docker',
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
